daae3692834b526a34bd1a0a9f1004bc7215a55d
QinYang Tan <108197858+TanQinYang@users.noreply.github.com>
2023-12-21 15:17:30 +0100

    Add files via upload

---
+++ Test data type convertion/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+/*{
+float pi=3.1415926;
+int s1=5;
+int r=5;
+double s2;
+s1=pi*r*r;
+s2=pi*r*r;
+
+    printf("%d\n",s1);
+    printf("%f",s2);
+    return 0;
+}
+*/
+{
+int sum=103;
+int count=7;
+double average;
+average=(double)sum/count;
+printf("%f",average);
+
+    return 0;
+}
